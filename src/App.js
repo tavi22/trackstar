ab02363482540be9548f6f357084eacf9095ed30
Octavian Mitrica <octavian.mitrica4@gmail.com>
2023-02-04 17:37:07 +0200

    created react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {useRef, useState, useEffect} from 'react';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
+function App() {
+
+  return (
+  <>
+  </>
+  );
+}
+
+
+
+export default App;
